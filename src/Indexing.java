7bf4c45683a72b123dd601c573653ac9e694a64d
darknight <icsd11175@icsd.aegean.gr>
2015-05-29 19:08:14 +0300

    Basic Design

---
+++ src/Indexing.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Konstantinos Chasiotis
+ */
+public class Indexing {
+    
+    public boolean Crawler(String url){
+        //Todo
+        return false;
+    }
+    
+    public void Run(){
+        //Todo
+    }
+    
+    public String[] loadUrls(){
+        
+    }
+    
+    public WordInfo indexOf(String Word){
+        
+    }
+}
---
+++ src/TextProcessing.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Konstantinos Chasiotis
+ */
+public class TextProcessing {
+
+    static String dummy;
+
+    public static String SanitizeText(String inputText) {
+        //Todo
+        return dummy;
+    }
+
+    public static String Stemmer(String inputText) {
+        //Todo
+        return dummy;
+    }
+
+    public static String toLowercase(String inputText) {
+        //Todo
+        return dummy;
+    }
+
+    public static String RemoveStopwords(String inputText) {
+        //Todo
+        return dummy;
+    }
+}
